ddd6aee5bdcb0f1702b93754c39799bdc142d862
priya2002hello <nikampriyanka2002@gmail.com>
2022-12-05 13:02:12 +0530

    Procfile added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
+const {StatusCodes}=require('http-status-codes');
+
 const express = require('express');
 const app = express();
 
@@ -21,6 +23,7 @@ const jobRouter=require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const { StatusCodes } = require('http-status-codes');
 
 app.use(express.json());
 // extra packages
@@ -35,13 +38,16 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get('/',(req,res)=>{
+  res.status(StatusCodes.OK).json({msg:"Hey there , I am Jobs Api",status :"success"});
+})
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',authenticateUser,jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
